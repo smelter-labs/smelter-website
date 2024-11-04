6ac7640042409bec4394ca001533670bea0a965e
BrtqKr <bartlomiej.krason@swmansion.com>
2024-11-04 12:22:39 +0100

    fix ts eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,12 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-import tsParser from '@typescript-eslint/parser';
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  {
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: ['tsconfig.json'],
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-      globals: globals.browser,
-    },
-  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginPrettierRecommended,
@@ -35,7 +21,6 @@ export default [
     rules: {
       'prettier/prettier': ['error'],
       '@typescript-eslint/no-explicit-any': [0, {}],
-      '@typescript-eslint/no-floating-promises': ['error'],
       'no-constant-condition': [1],
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
