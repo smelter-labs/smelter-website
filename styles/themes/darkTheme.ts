6871f2852889a5877a5a644942bcbffb83f6fc00
BrtqKr <bartlomiej.krason@swmansion.com>
2025-01-15 14:01:52 +0100

    format and lint files

--- styles/themes/darkTheme.ts
+++ styles/themes/darkTheme.ts
@@ -1,61 +1,61 @@
-import { COLORS } from '../colors';
+import { COLORS } from "../colors";
 
 export const DARK_THEME = {
-  colors: {
-    mainBackground: COLORS.black100,
-    nav: {
-      icon: COLORS.white100,
-      searchContent: COLORS.white100,
-      searchBorder: COLORS.white25,
-    },
-    hero: {
-      title: COLORS.white100,
-      subtitle: COLORS.purple20,
-      authorLabel: COLORS.white100,
-      docsButton: COLORS.white100,
-      docsGradientFrom: COLORS.red40,
-      docsGradientTo: COLORS.purple100,
-    },
-    gradient: {
-      red: COLORS.red40,
-      purple: COLORS.purple100,
-      redLight: COLORS.red0,
-      black: COLORS.black100,
-    },
-    explanation: {
-      header: COLORS.white100,
-      text: COLORS.white100,
-      background: COLORS.black100,
-    },
-    useCases: {
-      header: COLORS.white100,
-      subheader: COLORS.purple20,
-      tileHeader: COLORS.white100,
-      tileSubheader: COLORS.white100,
-      tileBackground: COLORS.black75,
-      borderInactive: COLORS.white25,
-    },
-    usage: {
-      header: COLORS.white100,
-      subheader: COLORS.purple20,
-      tileHeader: COLORS.white100,
-      tileText: COLORS.white100,
-      hoverBackground: COLORS.black75,
-    },
-    capabilities: {
-      header: COLORS.white100,
-      subheader: COLORS.purple20,
-      text: COLORS.white100,
-      border: COLORS.white25,
-    },
-    form: {
-      header: COLORS.white100,
-      border: COLORS.white25,
-      label: COLORS.white100,
-    },
-    footer: {
-      logo: COLORS.white100,
-      copyright: COLORS.white100,
-    },
-  },
+	colors: {
+		mainBackground: COLORS.black100,
+		nav: {
+			icon: COLORS.white100,
+			searchContent: COLORS.white100,
+			searchBorder: COLORS.white25,
+		},
+		hero: {
+			title: COLORS.white100,
+			subtitle: COLORS.purple20,
+			authorLabel: COLORS.white100,
+			docsButton: COLORS.white100,
+			docsGradientFrom: COLORS.red40,
+			docsGradientTo: COLORS.purple100,
+		},
+		gradient: {
+			red: COLORS.red40,
+			purple: COLORS.purple100,
+			redLight: COLORS.red0,
+			black: COLORS.black100,
+		},
+		explanation: {
+			header: COLORS.white100,
+			text: COLORS.white100,
+			background: COLORS.black100,
+		},
+		useCases: {
+			header: COLORS.white100,
+			subheader: COLORS.purple20,
+			tileHeader: COLORS.white100,
+			tileSubheader: COLORS.white100,
+			tileBackground: COLORS.black75,
+			borderInactive: COLORS.white25,
+		},
+		usage: {
+			header: COLORS.white100,
+			subheader: COLORS.purple20,
+			tileHeader: COLORS.white100,
+			tileText: COLORS.white100,
+			hoverBackground: COLORS.black75,
+		},
+		capabilities: {
+			header: COLORS.white100,
+			subheader: COLORS.purple20,
+			text: COLORS.white100,
+			border: COLORS.white25,
+		},
+		form: {
+			header: COLORS.white100,
+			border: COLORS.white25,
+			label: COLORS.white100,
+		},
+		footer: {
+			logo: COLORS.white100,
+			copyright: COLORS.white100,
+		},
+	},
 } as const;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,24 @@
-import { DARK_THEME } from './styles/themes/darkTheme';
+import { DARK_THEME } from "./styles/themes/darkTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    fontFamily: {
-      aeonik: ['Aeonik'],
-    },
-    extend: {
-      colors: {
-        ...DARK_THEME.colors,
-      },
-      spacing: {
-        17.5: '4.375rem',
-        18: '4.5rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      },
-    },
-  },
-  darkMode: 'selector',
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		fontFamily: {
+			aeonik: ["Aeonik"],
+		},
+		extend: {
+			colors: {
+				...DARK_THEME.colors,
+			},
+			spacing: {
+				17.5: "4.375rem",
+				18: "4.5rem",
+			},
+			borderRadius: {
+				"4xl": "2rem",
+			},
+		},
+	},
+	darkMode: "selector",
 };
