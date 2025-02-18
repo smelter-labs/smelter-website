926f71a833c8c97c174a89995272cc636fd0a284
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-02-18 15:13:19 +0100

    simplify example

--- src/content/docs/ts-sdk/components/examples/Show.tsx
+++ src/content/docs/ts-sdk/components/examples/Show.tsx
@@ -1,14 +1,14 @@
-import { InputStream, Show, Tiles } from "@swmansion/smelter";
+import { Mp4, Show, Tiles } from "@swmansion/smelter";
 import { OfflineSmelter } from "@swmansion/smelter-node";
 
 function ExampleApp() {
   return (
     <Tiles>
       <Show delayMs={2000}>
-        <InputStream inputId="input_1" />
+        <Mp4 source="https://example.com/video1.mp4" />
       </Show>
       <Show timeRangeMs={{ start: 5000, end: 8000 }}>
-        <InputStream inputId="input_2" />
+        <Mp4 source="https://example.com/video2.mp4" />
       </Show>
     </Tiles>
   );
@@ -18,35 +18,19 @@ async function run() {
   const smelter = new OfflineSmelter();
   await smelter.init();
 
-  await smelter.registerInput("input_1", {
+  await smelter.render(<ExampleApp />, {
     type: "mp4",
-    serverPath: "./inputExample1.mp4",
-    required: true,
-  });
-
-  await smelter.registerInput("input_2", {
-    type: "mp4",
-    serverPath: "./inputExample2.mp4",
-    required: true,
-  });
-
-  await smelter.render(
-    <ExampleApp />,
-    {
-      type: "mp4",
-      serverPath: "./output.mp4",
-      video: {
-        encoder: {
-          type: "ffmpeg_h264",
-          preset: "ultrafast",
-        },
-        resolution: {
-          width: 1920,
-          height: 1080,
-        },
+    serverPath: "./output.mp4",
+    video: {
+      encoder: {
+        type: "ffmpeg_h264",
+        preset: "ultrafast",
+      },
+      resolution: {
+        width: 1920,
+        height: 1080,
       },
     },
-    10000
-  );
+  });
 }
 void run();
