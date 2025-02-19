3c92218fc0c4dab13da2bf9aa32bc7f74ec092cd
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-02-19 17:30:56 +0100

    wip

--- src/content/docs/ts-sdk/components/examples/Shader.tsx
+++ src/content/docs/ts-sdk/components/examples/Shader.tsx
@@ -1,54 +1,58 @@
-import { Image, Shader } from "@swmansion/smelter";
+import { Mp4, Shader } from "@swmansion/smelter";
 import Smelter from "@swmansion/smelter-node";
 
-// Adds red border to input texture
-const RED_BORDER_SHADER = `
+const EXAMPLE_SHADER = `
 struct VertexInput {
-  @location(0) position: vec3<f32>,
-  @location(1) tex_coords: vec2<f32>,
+    @location(0) position: vec3<f32>,
+    @location(1) tex_coords: vec2<f32>,
 }
 
 struct VertexOutput {
-  @builtin(position) position: vec4<f32>,
-  @location(0) tex_coords: vec2<f32>,
+    @builtin(position) position: vec4<f32>,
+    @location(0) tex_coords: vec2<f32>,
+}
+
+@vertex
+fn vs_main(input: VertexInput) -> VertexOutput {
+    var output: VertexOutput;
+
+    output.position = vec4(input.position, 1.0);
+    output.tex_coords = input.tex_coords;
+
+    return output;
+}
+
+struct BaseShaderParameters {
+    plane_id: i32,
+    time: f32,
+    output_resolution: vec2<u32>,
+    texture_count: u32,
 }
 
 @group(0) @binding(0) var textures: binding_array<texture_2d<f32>, 16>;
 @group(2) @binding(0) var sampler_: sampler;
 
-struct BaseShaderParameters {
-  plane_id: i32,
-  time: f32,
-  output_resolution: vec2<u32>,
-  texture_count: u32,
-}
-
 var<push_constant> base_params: BaseShaderParameters;
-
-@vertex
-fn vs_main(input: VertexInput) -> VertexOutput {
-  var output: VertexOutput;
-
-  output.position = vec4(input.position.x, input.position.y, input.position.z, 1.0);
-  output.tex_coords = input.tex_coords;
-
-  return output;
-}
 
 @fragment
 fn fs_main(input: VertexOutput) -> @location(0) vec4<f32> {
-  let sample = textureSample(textures[0], sampler_, input.tex_coords);
-  let border_size = 50.0;
-
-  // input.position is interpolated into ([0, output_width], [0, output_height], ..) range
-  // by wgpu between vertex shader output and fragment shader input
-  if (input.position.x > border_size && input.position.x < f32(base_params.output_resolution.x) - border_size) {
-    if (input.position.y > border_size && input.position.y < f32(base_params.output_resolution.y) - border_size) {
-      return sample;
-    }
+  // Return transparent frame in case of different input video count
+  if (base_params.texture_count != 1u) {
+      return vec4(0.0, 0.0, 0.0, 0.0);
   }
 
-  return vec4(1.0, 0.0, 0.0, 1.0);
+  let pi = 3.14159;
+  let effect_radius = abs(sin(base_params.time) / 2.0);
+  let effect_angle = 2.0 * pi * abs(sin(base_params.time) / 2.0);
+
+  let center = vec2(0.5, 0.5);
+  let uv = input.tex_coords - center;
+
+  let len = length(uv);
+  let angle = atan2(uv.y, uv.x) + effect_angle * smoothstep(effect_radius, 0.0, len);
+  let coords = vec2(len * cos(angle), len * sin(angle)) + center;
+
+  return textureSample(textures[0], sampler_, coords);
 }
 `;
 
@@ -56,8 +60,8 @@ function ExampleApp() {
   return (
     <Shader
       shaderId="example_shader"
-      resolution={{ width: 1920, height: 1080 }}>
-      <Image source="https://mywebsite.com/example.svg" />
+      resolution={{ width: 1280, height: 720 }}>
+      <Mp4 source="https://example.com/video.mp4" />
     </Shader>
   );
 }
@@ -67,7 +71,7 @@ async function run() {
   await smelter.init();
 
   await smelter.registerShader("example_shader", {
-    source: RED_BORDER_SHADER,
+    source: EXAMPLE_SHADER,
   });
 
   await smelter.registerOutput("output", <ExampleApp />, {
