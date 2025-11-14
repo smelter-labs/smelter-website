51a77acc48f5d16b60ca272b8732909b819373e9
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2025-11-14 13:15:41 +0100

    Add `reCAPTCHA` (#196)
    Co-authored-by: piotrsnow <122814321+piotrsnow@users.noreply.github.com>

--- global.d.ts
+++ global.d.ts
@@ -2,4 +2,10 @@ export declare global {
   interface Window {
     clarity: (command: string, value?: boolean) => void;
   }
+
+  declare const grecaptcha: any;
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
 }
